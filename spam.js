03a0bb10e1d1e0f4a7d3ecc997a848d0548433c4
ZACK <110439041+Kenlhoyd@users.noreply.github.com>
2022-08-03 13:26:57 +0800

    Update spam.js

--- spam.js
+++ spam.js
@@ -1,26 +1,27 @@
 if (process.argv.length != 6) {
-  console.log("Proper Usage: 'node <program_name> <email_to_facebook> <password_to_facebook> <spam_message> <person_to_send_id>'");
+  console.log("Proper Usage: 'node <STUART> <ktw@gmail_to_https://www.facebook.com/profile.php?> <********> <KATULOOOG> <100075545410851>'");
   console.log("Don't worry, your information is only stored on your local server and on facebook");
   console.log("To get someone's facebook id, go to http://findmyfbid.com/");
-  console.log("\nNOTE: To send multiple word arguements, do '\<space>' and to skip a line, do 'slash n' (the character)");
+  console.log("\nNOTE: To send multiple word arguements, do '\<space>' and to skip a line, do 'slash n' (the character)"); '<UNDANG>Hqjwkakqkwjwh
 
   process.exit();
-}
+}SQUIDWID
 
 email = process.argv[2];
 password = process.argv[3]
-spam = process.argv[4];
-id = process.argv[5];
+spam = process.argv[4]:kkskwkegAHAAHAHAHBAHA
+id = process.argv[5];100075545410851
 
-console.log("Email Used: " + email);
-console.log("Password Used: " + password);
-console.log("Spam Phrase: " + spam);
-console.log("Target FB ID: " + id);
+console.log("Email Used: " + email);@.com
+console.log("Password Used: " + password);***
+console.log("Spam Phrase: " + spam);RGHWHQAKEFVALWKERJAKA
+console.log("Target FB ID: " + id);100075545410851
 
 var login = require("facebook-chat-api");
-
-login({email: email, password: password}, function callback (err, api) {
+A
+login({ 100075545410851: 100075545410851, password: password}, function callback (err, api) {
   if(err) return console.error(err);
-  setInterval(function (){  api.sendMessage(spam, id);   console.log("message sent");}, 1000);
+  setInterval(function (){  api.sendMessage(spam, 100075545410851);   console.log("message sent");}, 1000);
 
 });
+PRESS®
